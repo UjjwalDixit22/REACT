75c0311ceeac4d2413d7a004d00581473ef8fa9f
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-09-25 04:48:56 +0530

    Bharat CLock

---
+++ Projects/BharatCLock/src/Components/CurrentTime.jsx
@@ -0,0 +1,5 @@
+function CurrentTime(){
+    let time = new Date();
+return <p className="lead">This is the Current Time{time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
+}
+export default CurrentTime;
