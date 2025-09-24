b6826897ad51053805c7644919b6ccecb86511e8
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-09-25 05:27:37 +0530

    Fragments

---
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+function App() {
+  return (
+    <>
+        <h1> Healthy Foods</h1>
+        <ul class="list-group">
+          <li class="list-group-item">Blueberry</li>
+          <li class="list-group-item">Avacado</li>
+          <li class="list-group-item">Greek Yogurt</li>
+          <li class="list-group-item">Pineapple</li>
+          <li class="list-group-item">Chia Seeds</li>
+        </ul>
+    </>
+  );
+} 
+
+export default App ;
