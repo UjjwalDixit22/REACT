887ff48bdde2d490c79bcf2adb7c272518bdb2b8
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-09-25 06:10:01 +0530

    Conditional Rendering

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -3,15 +3,24 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  let foodITems = ['Blueberry', 'Avacadoes', 'Nuts', 'Pineapple'];
+  let foodITems =[];
+  // let foodITems = ['Blueberry', 'Avacadoes', 'Nuts', 'Pineapple'];
+
+  // if(foodITems.length===0){
+  //   return <p>Still Hungry!</p>
+  // }
+  // let emptyMessage = foodITems.length===0?<p>Still Hungry !!!</p> : null
+
   return (
     <>
       <h1>Healthy Foods</h1>
+      {/* {emptyMessage} */}
+      {foodITems.length === 0 && <p>Hey still there...</p>}
       <ul className="list-group">
         {foodITems.map((item) => (
           <li key={item} className="list-group-item">{item}</li>
           // Key is for if we want to change single item  you are giving id to enhance performance
-          
+
         ))} 
       </ul>
     </>
