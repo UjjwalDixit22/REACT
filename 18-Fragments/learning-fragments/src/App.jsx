9d5a2dc67b43789bcd25dc9f84749e7518458ddb
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-09-25 05:41:06 +0530

    Map Method

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -1,19 +1,21 @@
 import "./App.css";
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
+  let foodITems = ['Blueberry', 'Avacadoes', 'Nuts', 'Pineapple'];
   return (
     <>
-        <h1> Healthy Foods</h1>
-        <ul class="list-group">
-          <li class="list-group-item">Blueberry</li>
-          <li class="list-group-item">Avacado</li>
-          <li class="list-group-item">Greek Yogurt</li>
-          <li class="list-group-item">Pineapple</li>
-          <li class="list-group-item">Chia Seeds</li>
-        </ul>
+      <h1>Healthy Foods</h1>
+      <ul className="list-group">
+        {foodITems.map((item) => (
+          <li key={item} className="list-group-item">{item}</li>
+          // Key is for if we want to change single item  you are giving id to enhance performance
+          
+        ))} 
+      </ul>
     </>
   );
-} 
+}
 
-export default App ;
+export default App;
