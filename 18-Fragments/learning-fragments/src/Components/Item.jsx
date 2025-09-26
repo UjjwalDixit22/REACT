a97020a05e871b6cded1e943656ffeac11568b1a
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-09-27 02:06:12 +0530

    Props , Fragments , css Module

--- 18-Fragments/learning-fragments/src/Components/Item.jsx
+++ 18-Fragments/learning-fragments/src/Components/Item.jsx
@@ -1,5 +1,8 @@
+
+import styles from './Item.module.css';
+// We can name anything as parent object here in place of styles
 const Item = (props) => {
   //(inside bracket where props is written we can directly pass the name of the prop{foodItem})
-  return <li className="list-group-item ug-item"><span className="ug-span">{props.foodItem}</span></li>;
+  return <li className={`${styles["ug-item"]}`}><span className={`${styles["ug-span"]}`}>{props.foodItem}</span></li>;
 };
 export default Item;
