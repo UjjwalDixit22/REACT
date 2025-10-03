f02fe51a0ad775b0b23678d2259f91221e101a14
UjjwalDixit22 <ujjwaldixit285@gmail.com>
2025-10-04 03:45:30 +0530

    Managing State

--- 18-Fragments/learning-fragments/src/App.jsx
+++ 18-Fragments/learning-fragments/src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FoodItems from "./Components/FoodItems";
 import Errormsg from "./Components/Errormsg";
 import Container from "./Components/Container";
 import FoodInput from "./Components/FoodInput.";
-
+import { useState } from "react";
 
 function App() {
   // let foodITems =[];
@@ -15,11 +14,25 @@ function App() {
   //   return <p>Still Hungry!</p>
   // }
   // let emptyMessage = foodITems.length===0?<p>Still Hungry !!!</p> : null
-  let textToShow = "Food Item entered by the user." 
+  // let textStateArr =  useState("Food Item entered by the user.");
+  // let textToShow = textStateArr[0]; // Current Value
+  // let setTextStateFun = textStateArr[1]; // Changing Function
+
+  // Do this
+
+  let [textToShow, setTextStateFun] =  useState("Food Item entered by the user.");
+  console.log(`Current value of textState:${textToShow}`);
+  
+
+  // It returns array of current vale and changing function
+
+
+  // let textToShow = "Food Item entered by the user." 
   const handleOnchange=(e)=>{
-        console.log(e.target.value);
-        textToShow = e.target.value;
+        // console.log(e.target.value);
+        setTextStateFun(e.target.value);
     }
+
   return (
     <>
     <Container>
